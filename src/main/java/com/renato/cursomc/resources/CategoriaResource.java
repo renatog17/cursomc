468e9e596ea440528cd4ad0764ab8bf085976e8f
Renato Oliveira <renato.oliveira@ucsal.edu.br>
2020-05-02 00:16:50 -0300

    Criando reposity e service para categoria

--- src/main/java/com/renato/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/renato/cursomc/resources/CategoriaResource.java
@@ -3,27 +3,33 @@ package com.renato.cursomc.resources;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.renato.cursomc.domain.Categoria;
+import com.renato.cursomc.services.CategoriaService;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
+	@Autowired
+	private CategoriaService service;
 	
-	@RequestMapping(method = RequestMethod.GET)
-	public List<Categoria> listar() {
+	@RequestMapping(value="/{id}",method = RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		Categoria obj = service.buscar(id);
+		//Categoria cat1 = new Categoria(1, "Informática");
+		//Categoria cat2 = new Categoria(2, "Escitório");
 		
-		Categoria cat1 = new Categoria(1, "Informática");
-		Categoria cat2 = new Categoria(2, "Escitório");
+		//List<Categoria> lista = new ArrayList<>();
 		
-		List<Categoria> lista = new ArrayList<>();
+		//lista.add(cat1);
+		//lista.add(cat2);
 		
-		lista.add(cat1);
-		lista.add(cat2);
-		
-		return lista;
+		return ResponseEntity.ok().body(obj);
 	}
 }
